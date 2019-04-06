770634e4fdeda06fb9ba1879618da2bc6773db0e
Berthelmaster <berthel0802@gmail.com>
2019-04-06 16:48:49 +0200

    Added to AppDbcontext

---
+++ ConsoleApp10/Migrations/20190406144018_thirdMig.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ConsoleApp10.Migrations
+{
+    public partial class thirdMig : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Content",
+                columns: table => new
+                {
+                    Content_Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Content", x => x.Content_Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CourseContent",
+                columns: table => new
+                {
+                    Course_id = table.Column<int>(nullable: false),
+                    Content_id = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CourseContent", x => new { x.Course_id, x.Content_id });
+                    table.ForeignKey(
+                        name: "FK_CourseContent_Content_Content_id",
+                        column: x => x.Content_id,
+                        principalTable: "Content",
+                        principalColumn: "Content_Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CourseContent_Course_Course_id",
+                        column: x => x.Course_id,
+                        principalTable: "Course",
+                        principalColumn: "Course_id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CourseContent_Content_id",
+                table: "CourseContent",
+                column: "Content_id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CourseContent");
+
+            migrationBuilder.DropTable(
+                name: "Content");
+        }
+    }
+}
