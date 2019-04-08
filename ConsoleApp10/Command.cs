571b5867549160f60f6922443661331999420c9a
florent417 <flole@live.dk>
2019-04-08 15:45:32 +0200

    Added list for studentcourses and assigned to courses

--- ConsoleApp10/Command.cs
+++ ConsoleApp10/Command.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using Microsoft.EntityFrameworkCore;
 
@@ -32,5 +33,40 @@ namespace ConsoleApp10
                 }
             }
         }
+
+        public static async void ListStudentCourses(int studentId)
+        {
+            using (var db = new AppDbContext())
+            {
+                var studentCourses = await db.CourseEnrolledStudents.Where(s=>s.AU_id==studentId).ToListAsync();
+                Console.WriteLine("List of courses for students: \n");
+                foreach (var course in studentCourses)
+                {
+                    Console.WriteLine("Course with Course-Id found: {0}", course._Course.Name);
+                    Console.WriteLine("Status: {0}", course._Course.Status);
+                    Console.WriteLine("Grade for student: {0} \n", course.Grade);
+                }
+            }
+        }
+
+        public static async void ListAssignedToCourse(int courseId)
+        {
+            using (var db = new AppDbContext())
+            {
+                var courseStudents = await db.CourseEnrolledStudents.Where(c => c.Course_id == courseId).ToListAsync();
+                var courseTeachers = await db.CourseTeacher.Where(c => c.Course_id == courseId).ToListAsync();
+                Console.WriteLine("List of assigned students and teachers to course: \n");
+                foreach (var student in courseStudents)
+                {
+                    Console.WriteLine("Student name: {0}", student._Students.Name);
+                }
+
+                foreach (var teacher in courseTeachers)
+                {
+                    Console.WriteLine("\nTeacher name: {0}", teacher.Teacher.Name);
+                }
+            }
+        }
+
     }
 }
