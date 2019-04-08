8b190cbac19065bee980572c6816aeb507517032
zabih <44346668+mrching7@users.noreply.github.com>
2019-04-08 11:21:27 +0200

    Update GroupAssignment.cs

--- ConsoleApp10/Model/GroupAssignment.cs
+++ ConsoleApp10/Model/GroupAssignment.cs
@@ -8,6 +8,9 @@ namespace ConsoleApp10
 {
     public class GroupAssignment
     {
-
+        public int GroupID { get; set; }
+        public Group Group { get; set; }
+        public int Assignment_ID { get; set; }
+        public Assignment Assignment { get; set; }
     }
 }
