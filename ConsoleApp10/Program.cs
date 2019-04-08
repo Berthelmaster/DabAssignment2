28e67913ef9087841ddf938d8274724ed8e35242
Berthelmaster <berthel0802@gmail.com>
2019-04-08 15:47:19 +0200

    Added new implementation to program

--- ConsoleApp10/Program.cs
+++ ConsoleApp10/Program.cs
@@ -7,7 +7,7 @@ namespace ConsoleApp10
         static void Main(string[] args)
         {
             Console.WriteLine("Hello! You have the following Options: \n" +
-                              "Press 1 to List all students \n" + "Press 2 to list all Courses");
+                              "Press 1 to List all students \n" + "Press 2 to list all Courses \n" + "Press 3 to Search for student using AU_ID \n" + "Press 4 to list student and teachers assigned to a give Course \n");
             var choice = Console.ReadLine();
 
             switch (choice)
@@ -19,6 +19,18 @@ namespace ConsoleApp10
                 case "2":
                     Command.ListCourses();
                     Console.WriteLine("What do you want to do next? \n");
+                    break;
+                case "3":
+                    Console.WriteLine("Write AU_Id: ");
+                    var studentID = Convert.ToInt32(Console.ReadLine());
+                    Command.ListStudentCourses(studentID);
+                    break;
+                case "4":
+                    Console.WriteLine("Write Course_Id: ");
+                    var CourseID = Convert.ToInt32(Console.ReadLine());
+                    Command.ListStudentsAndTeachers(CourseID);
+                    break;
+                case "5":
                     break;
                 
             }
