4356209cc39f3864110d5c4213eb8cb27372fa79
zabih <44346668+mrching7@users.noreply.github.com>
2019-04-11 11:21:26 +0200

    Update AppDbContext.cs
    DummyData færdig

--- ConsoleApp10/Data/AppDbContext.cs
+++ ConsoleApp10/Data/AppDbContext.cs
@@ -228,6 +228,19 @@ namespace ConsoleApp10
             modelBuilder.Entity<Video>().HasData(new Video { Video_id = 101, Area_Id = 1200 });
             modelBuilder.Entity<Video>().HasData(new Video { Video_id = 102, Area_Id = 1900 });
 
+            //TextBlock 
+            modelBuilder.Entity<TextBlock>().HasData(new TextBlock { TextBlock_id = 201, Area_Id = 1900 });
+            modelBuilder.Entity<TextBlock>().HasData(new TextBlock { TextBlock_id = 202, Area_Id = 1200 });
+
+            //GroupSignUpLinks
+            modelBuilder.Entity<GroupSignUpLink>().HasData(new GroupSignUpLink { GSUL_id = 301, Area_Id = 1900 });
+            modelBuilder.Entity<GroupSignUpLink>().HasData(new GroupSignUpLink { GSUL_id = 302, Area_Id = 1200 });
+
+            //Audio
+            modelBuilder.Entity<Audio>().HasData(new Audio { Audio_id = 401, Area_Id = 1900 });
+            modelBuilder.Entity<Audio>().HasData(new Audio { Audio_id = 402, Area_Id = 1200 });
+
+
         }
 
 
